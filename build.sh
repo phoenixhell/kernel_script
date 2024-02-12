cde321b4b1f8e1194ea5e45de67467014c9776a4
hariphoenix1708 <138122182+hariphoenix1708@users.noreply.github.com>
2024-02-12 10:15:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -136,6 +136,14 @@ cfg_changes() {
    		elif [ $COMPILER == "aosp_clang" ]; then
 			sed -i 's/CONFIG_LTO_GCC=y/# CONFIG_LTO_GCC is not set/g' arch/arm64/configs/vendor/sweet_defconfig
 			sed -i 's/CONFIG_GCC_GRAPHITE=y/# CONFIG_GCC_GRAPHITE is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+   			sed -i 's/CONFIG_CC_STACKPROTECTOR_STRONG=y/# CONFIG_CC_STACKPROTECTOR_STRONG is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+		fi
+  	elif [[ $PROCS -le 4 && $TOTAL_RAM_GB -lt 8 ]]; then
+		echo -e "Detected $PROCS core CPU and $TOTAL_RAM_GB GB RAM, this will disable compiler optimizations."
+		if [ $COMPILER == "aosp_clang" ]; then
+			sed -i 's/CONFIG_LTO_GCC=y/# CONFIG_LTO_GCC is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+			sed -i 's/CONFIG_GCC_GRAPHITE=y/# CONFIG_GCC_GRAPHITE is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+   			sed -i 's/CONFIG_CC_STACKPROTECTOR_STRONG=y/# CONFIG_CC_STACKPROTECTOR_STRONG is not set/g' arch/arm64/configs/vendor/sweet_defconfig
 		fi
 	elif [[ $PROCS -le 4 && $TOTAL_RAM_GB -lt 8 ]]; then
 		echo -e "Detected $PROCS core CPU and $TOTAL_RAM_GB GB RAM, this will disable compiler optimizations."
