fd858e3e897c6af20a0b16d20198cad23d71c903
hariphoenix1708 <138122182+hariphoenix1708@users.noreply.github.com>
2024-02-22 18:11:25 +0530

    Update build-test.sh

--- build-test.sh
+++ build-test.sh
@@ -9,16 +9,15 @@
 
 # Set environment for directory
 WORKDIR="$(pwd)"
-KERNEL_GIT="https://gitlab.com/phoenix_clan/kernel_xiaomi_sweet"
+KERNEL_GIT="https://gitlab.com/phoenix_clan/phoenix_kernel"
 KERNEL_BRANCH="dev"
 KERNEL_DIR="$WORKDIR/Phoenix"
 IMG_DIR="$KERNEL_DIR"/out/arch/arm64/boot
 
+#Clone Kernel Source
 git clone --depth=1 $KERNEL_GIT -b $KERNEL_BRANCH $KERNEL_DIR
 
 cd $KERNEL_DIR
-
-#make clean && make mrproper
 
 # Get defconfig file
 DEFCONFIG=vendor/sweet_defconfig
@@ -30,15 +29,21 @@ export KBUILD_BUILD_USER="Harikumar"
 # Set if do you use GCC or clang compiler
 # Default is clang compiler
 #
-COMPILER=aosp_clang
+COMPILER=sd_clang
+GIT_CLANG=true
 
-GIT_CLANG=false
 # Get distro name
 DISTRO=$(source /etc/os-release && echo ${NAME})
 
 # Get all cores of CPU
 PROCS=$(nproc --all)
 export PROCS
+
+# Get total RAM
+RAM_INFO=$(free -m)
+TOTAL_RAM=$(echo "$RAM_INFO" | awk '/^Mem:/{print $2}')
+TOTAL_RAM_GB=$(awk "BEGIN {printf \"%.0f\", $TOTAL_RAM/1024}")
+export TOTAL_RAM_GB
 
 # Set date and time
 DATE=$(TZ=Asia/Jakarta date)
@@ -57,7 +62,6 @@ KERVER=$(make kernelversion)
 COMMIT_HEAD=$(git log --oneline -1)
 
 # Check directory path
-#if [ -d "/root/project" ]; then
 if [ -d "/root/project" ] || [ -d "/home/runner/work" ] || [ -d "/workspace/kernel_script" ]; then
 	echo -e "Detected Continuous Integration dir"
 	export LOCALBUILD=0
@@ -82,6 +86,11 @@ elif [ $LOCALBUILD == "1" ]; then
 	export KBUILD_BUILD_HOST=$(uname -a | awk '{print $2}')
 fi
 
+# Cleanup KernelSU first on local build
+if [[ -d "$KERNEL_DIR"/KernelSU && $LOCALBUILD == "1" ]]; then
+	rm -rf KernelSU drivers/kernelsu
+fi
+
 # Set function for telegram
 tg_post_msg() {
 	"${TELEGRAM_DIR}" -H -D \
@@ -98,13 +107,63 @@ tg_post_build() {
         "$2"
 }
 
-export ARCH=arm64
-export SUBARCH=ARM64
+# Set function for setup KernelSU
+setup_ksu() {
+	curl -kLSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
+	if [ -d "$KERNEL_DIR"/KernelSU ]; then
+		git apply KernelSU-hook.patch
+	else
+		echo -e "Setup KernelSU failed, stopped build now..."
+		exit 1
+	fi
+}
+
+# Set function for enable/disable compiler optimizations
+compiler_opt() {
+	if [[ $PROCS -gt 4 && $TOTAL_RAM_GB -ge 8 ]]; then
+		echo -e "Detected $PROCS core CPU and $TOTAL_RAM_GB GB RAM, this will enable compiler optimizations."
+		if [ $COMPILER == "clang" ]; then
+			sed -i 's/CONFIG_LTO_GCC=y/# CONFIG_LTO_GCC is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+			sed -i 's/CONFIG_GCC_GRAPHITE=y/# CONFIG_GCC_GRAPHITE is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+   		elif [ $COMPILER == "aosp_clang" ]; then
+			sed -i 's/CONFIG_LTO_GCC=y/# CONFIG_LTO_GCC is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+			sed -i 's/CONFIG_GCC_GRAPHITE=y/# CONFIG_GCC_GRAPHITE is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+   			sed -i 's/CONFIG_CC_STACKPROTECTOR_STRONG=y/# CONFIG_CC_STACKPROTECTOR_STRONG is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+		elif [ $COMPILER == "gcc" ]; then
+			sed -i 's/CONFIG_LTO=y/# CONFIG_LTO is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+			sed -i 's/CONFIG_LTO_CLANG=y/# CONFIG_LTO_CLANG is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+			sed -i 's/# CONFIG_LTO_NONE is not set/CONFIG_LTO_NONE=y/g' arch/arm64/configs/vendor/sweet_defconfig
+		fi
+  	elif [[ $PROCS -ge 4 && $TOTAL_RAM_GB -ge 8 ]]; then
+		echo -e "Detected $PROCS core CPU and $TOTAL_RAM_GB GB RAM, this will enable compiler optimizations."
+		if [ $COMPILER == "clang" ]; then
+			sed -i 's/CONFIG_LTO_GCC=y/# CONFIG_LTO_GCC is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+			sed -i 's/CONFIG_GCC_GRAPHITE=y/# CONFIG_GCC_GRAPHITE is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+   		elif [ $COMPILER == "aosp_clang" ]; then
+			sed -i 's/CONFIG_LTO_GCC=y/# CONFIG_LTO_GCC is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+			sed -i 's/CONFIG_GCC_GRAPHITE=y/# CONFIG_GCC_GRAPHITE is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+   			sed -i 's/CONFIG_CC_STACKPROTECTOR_STRONG=y/# CONFIG_CC_STACKPROTECTOR_STRONG is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+		elif [ $COMPILER == "gcc" ]; then
+			sed -i 's/CONFIG_LTO=y/# CONFIG_LTO is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+			sed -i 's/CONFIG_LTO_CLANG=y/# CONFIG_LTO_CLANG is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+			sed -i 's/# CONFIG_LTO_NONE is not set/CONFIG_LTO_NONE=y/g' arch/arm64/configs/vendor/sweet_defconfig
+		fi
+	elif [[ $PROCS -le 4 && $TOTAL_RAM_GB -lt 8 ]]; then
+		echo -e "Detected $PROCS core CPU and $TOTAL_RAM_GB GB RAM, this will disable compiler optimizations."
+		# Disable optimizations for Clang
+		sed -i 's/CONFIG_LTO=y/# CONFIG_LTO is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+		sed -i 's/CONFIG_LTO_CLANG=y/# CONFIG_LTO_CLANG is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+		sed -i 's/# CONFIG_LTO_NONE is not set/CONFIG_LTO_NONE=y/g' arch/arm64/configs/vendor/sweet_defconfig
+		# Disable optimizations for GCC
+		sed -i 's/CONFIG_LTO_GCC=y/# CONFIG_LTO_GCC is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+		sed -i 's/CONFIG_GCC_GRAPHITE=y/# CONFIG_GCC_GRAPHITE is not set/g' arch/arm64/configs/vendor/sweet_defconfig
+	fi
+}
 
 # Set function for cloning repository
 clone() {
 	# Clone AnyKernel3
-	git clone --depth=1 https://github.com/hariphoenix1708/AnyKernel3 -b dev
+	git clone --depth=1 https://github.com/hariphoenix1708/AnyKernel3.git -b dev
 
 	if [ $COMPILER == "clang" ]; then
 		# Clone Proton clang
@@ -138,15 +197,25 @@ clone() {
 		# Get path and compiler string
 		KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 		PATH=$TC_DIR/bin/:$GCC64_DIR/bin/:$GCC32_DIR/bin/:$PATH
+  	elif [ $COMPILER == "sd_clang" ] && [ "$GIT_CLANG" = true ]; then
+		# Clone GCC ARM64 and ARM32
+		git clone https://gitlab.com/ZyCromerZ/sdclang-16.1.0.1.git -b main --depth=1 clang
+		# Set environment for GCC ARM64 and ARM32
+  		TC_DIR=$KERNEL_DIR/clang
+		#GCC64_DIR=$KERNEL_DIR/clang/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+		#GCC32_DIR=$KERNEL_DIR/clang/gcc/linux-x86/arm/arm-linux-androideabi-4.9
+		# Get path and compiler string
+		KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+		PATH=$TC_DIR/bin/:$PATH
 	elif [ $COMPILER == "gcc" ]; then
 		# Clone GCC ARM64 and ARM32
-		git clone https://github.com/fiqri19102002/aarch64-gcc.git -b release/elf-12 --depth=1 gcc64
-		git clone https://github.com/fiqri19102002/arm-gcc.git -b release/elf-12 --depth=1 gcc32
+		git clone https://github.com/arter97/arm64-gcc.git --depth=1 gcc64
+		git clone https://github.com/arter97/arm32-gcc.git --depth=1 gcc32
 		# Set environment for GCC ARM64 and ARM32
 		GCC64_DIR=$KERNEL_DIR/gcc64
 		GCC32_DIR=$KERNEL_DIR/gcc32
 		# Get path and compiler string
-		KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1)
+		KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-none-elf-gcc --version | head -n 1)
 		PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
 	fi
 
@@ -155,63 +224,107 @@ clone() {
 
 # Set function for naming zip file
 set_naming() {
-	KERNEL_NAME="Phoenix-sweet-personal-$ZIP_DATE"
-	export ZIP_NAME="$KERNEL_NAME.zip"
+	if [ -d "$KERNEL_DIR"/KernelSU ]; then
+		KERNEL_NAME="Phoenix-sweet-ksu-personal-$ZIP_DATE"
+		export ZIP_NAME="$KERNEL_NAME.zip"
+	else
+		KERNEL_NAME="Phoenix-sweet-personal-$ZIP_DATE"
+		export ZIP_NAME="$KERNEL_NAME.zip"
+	fi
+}
+
+# Set function for send messages to Telegram
+send_tg_msg() {
+	tg_post_msg "<b>Docker OS: </b><code>$DISTRO</code>" \
+	            "<b>Kernel Version : </b><code>$KERVER</code>" \
+	            "<b>Date : </b><code>$DATE</code>" \
+	            "<b>Device : </b><code>Redmi Note 10 Pro (sweet)</code>" \
+	            "<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>" \
+	            "<b>Host CPU Name : </b><code>$CPU_NAME</code>" \
+	            "<b>Host Core Count : </b><code>$PROCS core(s)</code>" \
+	            "<b>Host RAM Count : </b><code>$TOTAL_RAM_GB GB</code>" \
+	            "<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>" \
+	            "<b>Branch : </b><code>$BRANCH</code>" \
+	            "<b>Last Commit : </b><code>$COMMIT_HEAD</code>"
 }
 
 # Set function for starting compile
 compile() {
 	echo -e "Kernel compilation starting"
-	if [ $LOCALBUILD == "0" ]; then
-		tg_post_msg "<b>Docker OS: </b><code>$DISTRO</code>" \
-		            "<b>Kernel Version : </b><code>$KERVER</code>" \
-		            "<b>Date : </b><code>$DATE</code>" \
-		            "<b>Device : </b><code>Redmi Note 10 Pro (sweet)</code>" \
-		            "<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>" \
-		            "<b>Host CPU Name : </b><code>$CPU_NAME</code>" \
-		            "<b>Host Core Count : </b><code>$PROCS</code>" \
-		            "<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>" \
-		            "<b>Branch : </b><code>$BRANCH</code>" \
-		            "<b>Last Commit : </b><code>$COMMIT_HEAD</code>"
-	fi
 	make O=out "$DEFCONFIG"
 	BUILD_START=$(date +"%s")
 	if [ $COMPILER == "clang" ]; then
- 		export ARCH=arm64
-   		export SUBARCH=ARM64
-		make -j"$PROCS" O=out \
-				CROSS_COMPILE=aarch64-linux-gnu- \
-				CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-				CC=clang \
-				AR=llvm-ar \
-				NM=llvm-nm \
-				LD=ld.lld \
-				OBJDUMP=llvm-objdump \
-				STRIP=llvm-strip
-    	elif [ $COMPILER == "aosp_clang" ]; then
-     		export ARCH=arm64
-   		export SUBARCH=ARM64
-		make -j"$PROCS" O=out \
-				CROSS_COMPILE=aarch64-linux-gnu- \
-				CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-				CC=clang \
-				AR=llvm-ar \
-				NM=llvm-nm \
-				LD=ld.lld \
-				OBJDUMP=llvm-objdump \
-				STRIP=llvm-strip
+		if [ $LOCALBUILD == "0" ]; then
+			make -j"$PROCS" O=out \
+					CROSS_COMPILE=aarch64-linux-gnu- \
+					LLVM=1
+		elif [ $LOCALBUILD == "1" ]; then
+			make -j"$PROCS" O=out \
+					CROSS_COMPILE=aarch64-linux-gnu- \
+					CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
+					CC=clang \
+					AR=llvm-ar \
+					NM=llvm-nm \
+					LD=ld.lld \
+					OBJDUMP=llvm-objdump \
+					STRIP=llvm-strip
+		fi
+  	elif [ $COMPILER == "aosp_clang" ]; then
+		if [ $LOCALBUILD == "0" ]; then
+			make -j"$PROCS" O=out \
+					CROSS_COMPILE=aarch64-linux-gnu- \
+					LLVM=1
+		elif [ $LOCALBUILD == "1" ]; then
+			make -j"$PROCS" O=out \
+					CROSS_COMPILE=aarch64-linux-gnu- \
+					CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
+					CC=clang \
+					AR=llvm-ar \
+					NM=llvm-nm \
+					LD=ld.lld \
+					OBJDUMP=llvm-objdump \
+					STRIP=llvm-strip
+		fi
+  	elif [ $COMPILER == "sd_clang" ]; then
+		if [ $LOCALBUILD == "0" ]; then
+			make -j"$PROCS" O=out \
+					CROSS_COMPILE=aarch64-linux-gnu- \
+					LLVM=1
+		elif [ $LOCALBUILD == "1" ]; then
+			make -j"$PROCS" O=out \
+					CROSS_COMPILE=aarch64-linux-gnu- \
+					CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
+					CC=clang \
+					AR=llvm-ar \
+					NM=llvm-nm \
+					LD=ld.lld \
+					OBJDUMP=llvm-objdump \
+					STRIP=llvm-strip
+		fi
 	elif [ $COMPILER == "gcc" ]; then
-		export CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-eabi-
-		make -j"$PROCS" O=out CROSS_COMPILE=aarch64-elf-
+		export CROSS_COMPILE_COMPAT=$GCC32_DIR/bin/arm-none-eabi-
+		make -j"$PROCS" O=out CROSS_COMPILE=aarch64-none-elf-
 	fi
 	BUILD_END=$(date +"%s")
 	DIFF=$((BUILD_END - BUILD_START))
 	if [ -f "$IMG_DIR"/Image.gz-dtb ]; then
 		echo -e "Kernel successfully compiled"
+		if [ $LOCALBUILD == "1" ]; then
+			git restore arch/arm64/configs/vendor/sweet_defconfig
+			if [ -d "$KERNEL_DIR"/KernelSU ]; then
+				git restore drivers/ fs/
+			fi
+		fi
 	elif ! [ -f "$IMG_DIR"/Image.gz-dtb ]; then
 		echo -e "Kernel compilation failed"
 		if [ $LOCALBUILD == "0" ]; then
 			tg_post_msg "<b>Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>"
+		fi
+		if [ $LOCALBUILD == "1" ]; then
+			git restore arch/arm64/configs/vendor/sweet_defconfig
+			if [ -d "$KERNEL_DIR"/KernelSU ]; then
+				git restore drivers/ fs/
+			fi
 		fi
 		exit 1
 	fi
@@ -219,7 +332,7 @@ compile() {
 
 # Set function for zipping into a flashable zip
 gen_zip() {
-	if [ $LOCALBUILD == "1" ]; then
+	if [[ $LOCALBUILD == "1" || -d "$KERNEL_DIR"/KernelSU ]]; then
 		cd AnyKernel3 || exit
 		rm -rf dtbo/oss/dtbo.img dtb.img Image.gz-dtb *.zip
 		cd ..
@@ -249,6 +362,15 @@ gen_zip() {
 }
 
 clone
+compiler_opt
+if [ $LOCALBUILD == "0" ]; then
+	send_tg_msg
+fi
+compile
+set_naming
+gen_zip
+setup_ksu
+compiler_opt
 compile
 set_naming
 gen_zip
